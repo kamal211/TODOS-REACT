f73a1da33fbaf06f271b16c4acddbdc5fc06dfae
kamal211 <92024406+kamal211@users.noreply.github.com>
2022-05-19 13:46:48 +0100

    Update TodoForm.js

--- TodoForm.js
+++ TodoForm.js
@@ -1,7 +1,13 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 
 function TodoForm(props) {
-    const [input , setInput] = useState('');
+    const [input , setInput] = useState(props.edit ? props.edit.value : '' );
+    const inputRef = useRef(null);
+    
+    useEffect(() => {
+      inputRef.current.focus();
+    }
+    );
 
     const handleChange = e =>{
       setInput(e.target.value);
@@ -19,15 +25,37 @@ function TodoForm(props) {
     };
   return (
     <form className='todo-form' onSubmit={handleSubmit}>
+      { props.edit ? (
+        <>
         <input
-        type='text'
-        placeholder='Add a todo'
+    
+        placeholder='Update your item '
         value={input}
-        name='text'
-        className='todo-input'
         onChange={handleChange}
+        name='text'
+        className='todo-input edit'
+        ref={inputRef}
         />
-        <button className='todo-button'>ADD TODO</button>
+        <button onClick={handleSubmit} className='todo-button'>
+          Update
+        </button>
+        </>
+      ) : (
+        <>
+<input
+    
+    placeholder='Add a todo'
+    value={input}
+    onChange={handleChange}
+    name='text'
+    className='todo-input'
+    ref={inputRef}
+    />
+    <button onClick={handleSubmit} className='todo-button'>
+      Add todo
+    </button>
+</>
+      )}
     </form>
   );
 }
